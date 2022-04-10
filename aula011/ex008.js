1c2fd138d10f2c7f72816468d30364f118295c73
Rafael Silva de Aguiar <rfael.saguiar@gmail.com>
2022-04-10 13:38:33 -0300

    comecei

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A celocidade  do seu carro é de ${vel}km/h`)
+if(vel > 60){//condição simples!
+    console.log(`voce ultrapassou a veloxidade permitida.MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
---
+++ aula011/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'França'
+console.log(`Vivendo em ${pais}`)
+if(pais=='Brasil'){
+    console.log('Brasileira')
+}
+else{
+    console.log('Estrangeira')
+}
